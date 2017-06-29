72536524d460bb422a6e6eb928c2ba078b96b0a5
Xavier Yvez <xavieryvez@gmail.com>
2017-06-29 07:01:41 +0200

    Ledger cont. (#7)
    * Circle with libusb headers
    
    * Ledger signTransaction
    
    * Flow fixes/types

--- packages/theblock-lib-hw/flow-types/ledger.js
+++ packages/theblock-lib-hw/flow-types/ledger.js
@@ -1,16 +1,13 @@
 // GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
 // @flow
 
-import type { LedgerComms, LedgerEth } from '../src/types'; // eslint-disable-line
+import type { LedgerEthComms, LedgerEth, U2FCommsType } from '../src/types'; // eslint-disable-line
 
 declare module 'ledgerco' {
   declare var exports: {
-    comm_u2f: {
-      create_async: () => Promise<LedgerComms>
-    },
-
+    comm_u2f: U2FCommsType,
     eth: {
-      constructor (comms: LedgerComms): LedgerEth;
+      constructor (comms: LedgerEthComms): LedgerEth;
     }
   }
 }
--- packages/theblock-lib-hw/src/ledger.js
+++ packages/theblock-lib-hw/src/ledger.js
@@ -1,11 +1,14 @@
 // GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
 // @flow
 
-// import EthereumTx from 'ethereumjs-tx';
+import EthereumTx from 'ethereumjs-tx';
 import ledger from 'ledgerco';
 import u2f from 'u2f-api';
 
-import type { LedgerComms, LedgerEth, LedgerResultGetAddressType, U2FApiResultType } from './types';
+import type { TransactionType } from 'theblock-lib-util/src/types';
+import type { LedgerEthComms, LedgerEth, LedgerResultGetAddressType, LedgerResultSignType, U2FApiResultType } from './types';
+
+import { createRawTransaction } from 'theblock-lib-util/src/transaction';
 
 const PATH_ETC = "44'/60'/160720'/0'/0";
 const PATH_ETH = "44'/60'/0'/0";
@@ -15,8 +18,6 @@ if (window.u2f === undefined) {
 }
 
 export default class Ledger {
-  _instance: ?LedgerEth = null;
-
   path (chainId: number) {
     switch (chainId) {
       case 61:
@@ -28,16 +29,10 @@ export default class Ledger {
   }
 
   createLedgerInstance (): Promise<LedgerEth> {
-    if (this._instance) {
-      return Promise.resolve(this._instance);
-    }
-
     return ledger.comm_u2f
       .create_async()
-      .then((connection: LedgerComms) => {
-        this._instance = new ledger.eth(connection); // eslint-disable-line new-cap
-
-        return this._instance;
+      .then((connection: LedgerEthComms) => {
+        return new ledger.eth(connection); // eslint-disable-line new-cap
       })
       .catch((error: Error) => {
         console.error('Ledger:createLedgerInstance', error);
@@ -46,20 +41,39 @@ export default class Ledger {
       });
   }
 
-  destroyLedgerInstance () {
-    this._instance = null;
+  destroyLedgerInstance (instance: LedgerEth): Promise<boolean> {
+    if (!instance) {
+      return Promise.resolve(true);
+    }
+
+    return instance.comm
+      .close_async()
+      .catch((error) => {
+        console.error('Ledger:destroyLedgerInstance', error);
+      })
+      .then(() => {
+        return true;
+      });
   }
 
   getAddresses (chainId: number): Promise<Array<string>> {
     return this
       .createLedgerInstance()
       .then((instance: LedgerEth) => {
-        return instance.getAddress_async(this.path(chainId), true, false);
-      })
-      .then((result: LedgerResultGetAddressType) => {
-        console.log('Ledger:getAddresses', result);
+        return instance
+          .getAddress_async(this.path(chainId), true, false)
+          .then(({ address }: LedgerResultGetAddressType) => {
+            console.log('Ledger:getAddresses', address);
 
-        return [result.address];
+            return [address];
+          })
+          .then((addresses) => {
+            return this
+              .destroyLedgerInstance(instance)
+              .then(() => {
+                return addresses;
+              });
+          });
       })
       .catch((error: Error) => {
         console.error('Ledger:getAddresses', error);
@@ -68,24 +82,56 @@ export default class Ledger {
       });
   }
 
+  signTransaction (transaction: TransactionType): Promise<string> {
+    const tx: EthereumTx = createRawTransaction(transaction);
+
+    return this
+      .createLedgerInstance()
+      .then((instance: LedgerEth) => {
+        return instance
+          .signTransaction_async(
+            this.path(transaction.chainId),
+            tx.serialize().toString('hex')
+          )
+          .then(({ r, s, v }: LedgerResultSignType) => {
+            tx.r = Buffer.from(r, 'hex');
+            tx.s = Buffer.from(s, 'hex');
+            tx.v = Buffer.from(v, 'hex');
+
+            const txRaw: string = `0x${tx.serialize().toString('hex')}`;
+
+            return this
+              .destroyLedgerInstance(instance)
+              .then(() => {
+                return txRaw;
+              });
+          });
+      })
+      .catch((error: Error) => {
+        console.error('Ledger:signTransaction', error);
+
+        throw error;
+      });
+  }
+
   static isU2FAvailable (): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      if (window.u2f && !window.u2f.getApiVersion) {
+      if (!window.u2f.getApiVersion) {
         console.log('Ledger:isU2FAvailable', 'supported, no getApiVersion');
 
-        resolve(true);
+        return resolve(true);
       }
 
       u2f.getApiVersion((version: Error | U2FApiResultType) => {
         if (!version.js_api_version) {
           console.error('Ledger:isU2FAvailable', version);
 
-          reject(version);
-        } else {
-          console.log('Ledger:isU2FAvailable', 'available with', version);
-
-          resolve(true);
+          return reject(version);
         }
+
+        console.log('Ledger:isU2FAvailable', 'available with', version);
+
+        resolve(true);
       }, 1000);
     });
   }
