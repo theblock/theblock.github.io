2178a9eaae35fac353629de9a2f79b62c6663626
xavieryvez <xavieryvez@users.noreply.github.com>
2017-07-03 22:57:33 +0200

    Concat tests

--- packages/theblock-lib-util/src/format.spec.js
+++ packages/theblock-lib-util/src/format.spec.js
@@ -2,11 +2,25 @@
 // @flow
 
 import { NULL_ADDRESS } from './constants';
-import { formatAddress } from './format';
+import { concatHex, formatAddress } from './format';
 
 const ADDRESS = '0x00a329c0648769A73afAc7F9381E08FB43dBEA72';
 
 describe('format', () => {
+  describe('concatHex', () => {
+    it('concats removing extra 0x', () => {
+      expect(concatHex(['0x12', '0x34'])).toBe('0x1234');
+    });
+
+    it('concats in absense of 0x', () => {
+      expect(concatHex(['12', '34'])).toBe('0x1234');
+    });
+
+    it('concats wih null values present', () => {
+      expect(concatHex(['0x12', null, '0x34'])).toBe('0x1234');
+    });
+  });
+
   describe('formatAddress', () => {
     it('resturns 0x00..00 for no address', () => {
       expect(formatAddress()).toBe(NULL_ADDRESS);
