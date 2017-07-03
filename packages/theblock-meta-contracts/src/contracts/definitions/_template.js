b782c4961570f7b205e1c881f4b94fdd38901e28
xavieryvez <xavieryvez@users.noreply.github.com>
2017-07-03 09:05:21 +0200

    Add ENS interfaces

---
+++ packages/theblock-meta-contracts/src/contracts/definitions/_template.js
@@ -0,0 +1,17 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { ContractType } from '../../types';
+
+export default ({
+  image: 'question-128.png',
+  key: 'KEY',
+  name: 'NAME',
+  url: 'https://',
+  where: [
+    {
+      address: '0x',
+      chainId: 1
+    }
+  ]
+}: ContractType);
---
+++ packages/theblock-meta-contracts/src/contracts/definitions/ensRegistrar.js
@@ -0,0 +1,25 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { ContractType } from '../../types';
+
+export default ({
+  image: 'question-128.png',
+  key: 'ENSRegistrar',
+  name: 'ENS Registrar',
+  url: 'https://ens.domains/',
+  where: [
+    {
+      address: '0x314159265dD8dbb310642f98f50C066173C1259b',
+      chainId: 1
+    },
+    {
+      address: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+      chainId: 3
+    },
+    {
+      address: '0xe7410170f87102DF0055eB195163A03B7F2Bff4A',
+      chainId: 4
+    }
+  ]
+}: ContractType);
