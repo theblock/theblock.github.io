0652fb8546efd390ad5ca2d65333e4b6c5270753
xavieryvez <xavieryvez@users.noreply.github.com>
2017-07-10 11:00:55 +0200

    Add Lunyr

---
+++ packages/meta-contracts/src/tokens/definitions/lunyr.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 18,
+  image: 'lunyr-98.png',
+  key: 'LUN',
+  name: 'Lunyr',
+  url: 'https://lunyr.com',
+  where: [
+    {
+      address: '0xfa05A73FfE78ef8f1a739473e462c54bae6567D9',
+      chainId: 1
+    }
+  ]
+}: TokenType);
