db4e0fbf45af142e369fa7066c646550c289afbf
xavieryvez <xavieryvez@users.noreply.github.com>
2017-07-11 20:38:26 +0200

    Sha3 default pattern seed

--- packages/lib-ui/src/img/pattern/store.js
+++ packages/lib-ui/src/img/pattern/store.js
@@ -7,7 +7,7 @@ import type { StorageNameType } from '@theblock/lib-util/src/types';
 
 import { getStorage, setStorage } from '@theblock/lib-util/src/storage';
 
-const DEFAULT_SEED: string = '1499771680724';
+const DEFAULT_SEED: string = '0x614a94c05b231269918a0a6265f80b3d5209835d710e0e0022f44a5e9d17cd6f';
 const LS_BACKGROUND: StorageNameType = 'background';
 const defaults: { seed: string } = getStorage(LS_BACKGROUND);
 
