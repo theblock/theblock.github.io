80b1fc05b4d3e9b8443fb6b81c75f3927596f86d
xavieryvez <xavieryvez@users.noreply.github.com>
2017-07-12 09:26:41 +0200

    Rename typed module

---
+++ flow-typed/ledgerco.js
@@ -0,0 +1,13 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { LedgerEthComms, LedgerEth, U2FCommsType } from '../src/types'; // eslint-disable-line
+
+declare module 'ledgerco' {
+  declare var exports: {
+    comm_u2f: U2FCommsType,
+    eth: {
+      constructor (comms: LedgerEthComms): LedgerEth;
+    }
+  }
+}
