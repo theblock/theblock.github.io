0487a14ce73f49a04134a8e4125166a7fcceb964
Xavier Yvez <xavieryvez@gmail.com>
2017-07-18 00:52:52 +0200

    Add additional tokens (#50)
    * Add Lunyr
    
    * Add ciciv
    
    * Spelling

---
+++ packages/meta-contracts/src/tokens/definitions/civic.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 8,
+  image: 'civic-128.png',
+  key: 'CVC',
+  name: 'Civic',
+  url: 'https://www.civic.com',
+  where: [
+    {
+      address: '0x41e5560054824ea6b0732e656e3ad64e20e94e45',
+      chainId: 1
+    }
+  ]
+}: TokenType);
