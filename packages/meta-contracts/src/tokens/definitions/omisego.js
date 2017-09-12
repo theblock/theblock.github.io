6b704daa88c34ad2c80745e4ae56a0c9b74281ea
Xavier Yvez <xavieryvez@gmail.com>
2017-09-12 16:06:38 +0200

    Add OMG & Kin tokens (#55)
    * Add KikKin & OmiseGo
    
    * Update chainId to Homestead

---
+++ packages/meta-contracts/src/tokens/definitions/omisego.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 18,
+  image: 'omisego-128.png',
+  key: 'OMG',
+  name: 'OmiseGo',
+  url: 'https://omg.omise.co/',
+  where: [
+    {
+      address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07',
+      chainId: 1
+    }
+  ]
+}: TokenType);
