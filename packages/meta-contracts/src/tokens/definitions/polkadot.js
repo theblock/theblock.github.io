a197e57bcb06f2379368dd14148c7e0cf6566e15
xavieryvez <xavieryvez@users.noreply.github.com>
2017-10-17 10:16:36 +0200

    Add Aion & Polkadot

---
+++ packages/meta-contracts/src/tokens/definitions/polkadot.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 3,
+  image: 'polkadot-64.png',
+  key: 'DOT',
+  name: 'Polkadot',
+  url: 'https://polkadot.network',
+  where: [
+    {
+      address: '0xb59f67a8bff5d8cd03f6ac17265c550ed8f33907',
+      chainId: 1
+    }
+  ]
+}: TokenType);
