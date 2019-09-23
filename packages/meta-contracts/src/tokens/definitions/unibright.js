0f7213e07edd59222e27ee29df75203ffa43f9af
Xavier Yvez <xavieryvez@gmail.com>
2019-09-23 08:37:27 +0200

    Add UniBright (#61)
    * Add UniBright
    
    * Build

---
+++ packages/meta-contracts/src/tokens/definitions/unibright.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 8,
+  image: 'question-128.png',
+  key: 'UBT',
+  name: 'UniBright',
+  url: 'https://unibright.io/',
+  where: [
+    {
+      address: '0x8400d94a5cb0fa0d041a3788e395285d61c9ee5e',
+      chainId: 1
+    }
+  ]
+}: TokenType);
