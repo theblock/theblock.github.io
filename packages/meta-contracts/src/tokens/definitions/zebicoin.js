bdd46ba7fa4ab70b49c42c6d38c7b8c84a30a842
Xavier Yvez <xavieryvez@gmail.com>
2019-09-24 20:57:16 +0200

    Add Zebi

---
+++ packages/meta-contracts/src/tokens/definitions/zebicoin.js
@@ -0,0 +1,18 @@
+// GPLv3, Copyright (C) 2017, theBlock, https://theblock.io
+// @flow
+
+import type { TokenType } from '../../types';
+
+export default ({
+  decimals: 8,
+  image: 'question-128.png',
+  key: 'ZCO',
+  name: 'Zebi',
+  url: 'https://www.zebi.io/',
+  where: [
+    {
+      address: '0x2008e3057bd734e10ad13c9eae45ff132abc1722',
+      chainId: 1
+    }
+  ]
+}: TokenType);
